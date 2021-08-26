636cfcd5673d6cd4528166b2c1dee06bc18d92f6
Elektra Murphy <57063146+elektramurphy@users.noreply.github.com>
2021-08-26 17:27:32 +0100

    Updated solution for hero banner

--- git-task/src/components/homepage/hero.js
+++ git-task/src/components/homepage/hero.js
@@ -3,15 +3,45 @@ import Nav from "./nav"
 
 function Hero() {
     return (
-        <div className="hero">
-            <div className="h-full">
-                <div className="max-w-6xl m-auto block md:pt-10">
+        <>
+            <div className="hidden md:block hero h-full">
+                <div className="max-w-6xl m-auto block pt-10">
                     <Nav />
                     <div>
-                        <div className="text-center lg:text-left px-5 md:pl-10 pt-10 md:pt-24">
-                            <h1>Make smarter decisions</h1>
+                        <div className="text-left px-5 pl-10 pt-24">
+                            <h1 className="text-xl">Make smarter decisions</h1>
                             <h2>Ravelin provides technology and support to help you prevent <br></br> evolving fraud threats and accept payments with confidence.</h2>
-                            <div className="relative flex justify-center lg:justify-start mt-5">
+                            <div className="relative flex justify-start mt-5 pb-48">
+                                <a
+                                    href="https://www.ravelin.com/solutions/fraud"
+                                    rel="noopener"
+                                    className="inline-block py-3 px-8 rounded-md bg-blue-light text-white cursor-pointer"
+                                >
+                                    <span>View solutions</span>
+                                </a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <div>
+                    <img
+                        className="hidden xl:grid z-10 relative ml-auto pointer-events-none"
+                        width={'514px'}
+                        height={'620px'}
+                        style={{ maxHeight: 620, marginTop: -540, }}
+                        src="/assets/product-fragments.png"
+                        alt="ravelin dashboard"
+                    />
+                </div>
+                <div className="md:hidden heromobile h-full">
+                    <Nav />
+                    <div>
+                        <div className="text-center px-5 py-24">
+                            <h1 className="text-xl">Make smarter decisions</h1>
+                            <h2>Ravelin provides technology and support to help you prevent <br></br> evolving fraud threats and accept payments with confidence.</h2>
+                            <div className="relative flex justify-center mt-5">
                                 <a
                                     href="https://www.ravelin.com/solutions/fraud"
                                     rel="noopener"
@@ -19,20 +49,11 @@ function Hero() {
                                 >
                                     <span>View solutions</span>
                                 </a>
-                            </div>
+                            </div>    
                         </div>
                     </div>
                 </div>
-                <img
-                    className="z-10 relative ml-auto lg:-pt-96 pointer-events-none"
-                    width={'514px'}
-                    height={'620px'}
-                    style={{ maxHeight: 620 }}
-                    src="/assets/product-fragments.png"
-                    alt="ravelin dashboard"
-                />
-            </div>
-        </div>
+        </>
     );
 }
 
