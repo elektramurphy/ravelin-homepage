7fc14d1fca38fb019f28283d144aeaf22622eb82
Elektra Murphy <57063146+elektramurphy@users.noreply.github.com>
2021-08-27 10:27:46 +0100

    Config file adjusted to include styles in purge path

--- git-task/tailwind.config.js
+++ git-task/tailwind.config.js
@@ -5,7 +5,11 @@ const defaultFontStack = [
 ];
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     
