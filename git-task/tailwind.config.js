13506729239079b6fa8c57c67bd38d5e094cf7d7
Elektra Murphy <57063146+elektramurphy@users.noreply.github.com>
2021-08-27 14:44:53 +0100

    Revert "Config file adjusted to include styles in purge path"
    This reverts commit 7fc14d1fca38fb019f28283d144aeaf22622eb82.

--- git-task/tailwind.config.js
+++ git-task/tailwind.config.js
@@ -5,11 +5,7 @@ const defaultFontStack = [
 ];
 
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}', 
-    './components/**/*.{js,ts,jsx,tsx}',
-    './styles/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     
